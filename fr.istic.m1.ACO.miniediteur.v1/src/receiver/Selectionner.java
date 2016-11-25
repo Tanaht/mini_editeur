8f0f32b165e8bcec96d542737888a01ef72cba52
LouisA7 <a.louis.roland@gmail.com>
2016-11-25 16:03:20 +0100

    Javadoc Selectionner

--- fr.istic.m1.ACO.miniediteur.v1/src/receiver/Selectionner.java
+++ fr.istic.m1.ACO.miniediteur.v1/src/receiver/Selectionner.java
@@ -1,15 +1,25 @@
 package receiver;
 
+/**
+ * La commande Selectionner, sert à selectionner une sous partie du buffer, ou placer le curseur
+**/
 public class Selectionner implements Command {
 	private Receiver receiver;
 	private IHM ihm;
 	
+	/**
+	 * Le constructeur de la classe Selectionner
+	 * @param receiver le moteur
+	 * @param ihm l'interface utilisateur
+	**/
 	public Selectionner(Receiver receiver, IHM ihm) {
 		this.receiver = receiver;
 		this.ihm = ihm;
 	}
 
-	
+	/**
+	 * Récupère la sélection dans l'IHM et la transfère au moteur
+	**/
 	@Override
 	public void execute() {
 		this.receiver.selectionner(this.ihm.getSelection());
