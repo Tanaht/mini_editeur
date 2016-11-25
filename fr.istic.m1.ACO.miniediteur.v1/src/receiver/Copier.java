8147c9a0c73f5c8890aba94a0de98446b857a681
LouisA7 <a.louis.roland@gmail.com>
2016-11-25 15:51:28 +0100

    Javadoc Copier

--- fr.istic.m1.ACO.miniediteur.v1/src/receiver/Copier.java
+++ fr.istic.m1.ACO.miniediteur.v1/src/receiver/Copier.java
@@ -1,13 +1,22 @@
 package receiver;
 
+/**
+ * La commande Copier, sert à copier
+**/
 public class Copier implements Command {
 
 	private Receiver receiver;
 	
+	/**
+	 * Constructeur de la classe Copier
+	**/
 	public Copier(Receiver receiver) {
 		this.receiver = receiver;
 	}
 	
+	/**
+	 * Appelle la methode copier du moteur
+	**/
 	@Override
 	public void execute() {
 		receiver.copier();
